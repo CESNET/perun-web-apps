34202e2e6ac7b69883d71f5bda3b8ff3c0162afd
Martin Kostka <mkostka62@gmail.com>
2020-02-06 12:59:21 +0100

    WIP
    * Added very basic side-menu for testing purposes

--- apps/user-profile/src/app/pages/groups-page/groups-page.component.ts
+++ apps/user-profile/src/app/pages/groups-page/groups-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthzService, MembersService, UsersService } from '@perun-web-apps/perun/services';
+import { Group, GroupsManagerService } from '@perun-web-apps/perun/openapi';
+import { PerunPrincipal, Vo } from '@perun-web-apps/perun/models';
 
 @Component({
   selector: 'perun-web-apps-groups-page',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupsPageComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  principal: PerunPrincipal;
+  vos: Vo[] = [];
+  membersGroups: Group[] = [];
+  adminsGroups: Group[] = [];
+
+  constructor(private usersService: UsersService,
+              private memberService: MembersService,
+              private groupService: GroupsManagerService,
+              private authzService:AuthzService) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.authzService.getPerunPrincipal().subscribe(principal =>{
+      this.principal = principal;
+      console.log(principal);
+      // this.refreshTable();
+    });
   }
+
+  // refreshTable() {
+  //   this.loading = true;
+  //   this.membersGroups = [];
+  //   this.usersService.getVosWhereUserIsMember(this.principal.userId).subscribe(vos => {
+  //     this.vos = vos;
+  //     for(const vo of this.vos) {
+  //       this.memberService.getMemberByUser(vo.id, this.principal.userId).subscribe( member => {
+  //         this.groupService.getMemberGroups(member.id).subscribe( groups => {
+  //           this.membersGroups = this.membersGroups.concat(groups);
+  //         });
+  //       });
+  //     }
+  //     this.loading = false;
+  //   });
+  //   this.usersService.getGroupsWhereUserIsAdmin(this.principal.userId).subscribe( groups => {
+  //     this.adminsGroups = groups;
+  //   });
+  // }
 
 }
