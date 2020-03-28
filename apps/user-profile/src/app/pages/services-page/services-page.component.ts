3821b09f8235a8f8f44d7998f7391d2c23d8f53a
Martin Kostka <mkostka62@gmail.com>
2020-03-28 16:54:45 +0100

    Services page works
    * Services page was implemented
    * Resourcelist component will be moved to shared lib in next commit/s

--- apps/user-profile/src/app/pages/services-page/services-page.component.ts
+++ apps/user-profile/src/app/pages/services-page/services-page.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  MembersManagerService,
+  ResourcesManagerService,
+  RichResource,
+  UsersManagerService,
+  Vo
+} from '@perun-web-apps/perun/openapi';
+import { StoreService } from '@perun-web-apps/perun/services';
 
 @Component({
   selector: 'perun-web-apps-services-page',
@@ -7,9 +15,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private usersManagerService: UsersManagerService,
+              private membersManagerService: MembersManagerService,
+              private resourcesManagerService: ResourcesManagerService,
+              private storage: StoreService) { }
+
+  vos: Vo[];
+  userId: number;
+  resources: RichResource[] = [];
+
+  hiddenColumns: string[] = ['select', 'id', 'facility', 'tags'];
 
   ngOnInit() {
+    this.userId = this.storage.getPerunPrincipal().userId;
+    this.usersManagerService.getVosWhereUserIsMember(this.userId).subscribe(vos => {
+      this.vos = vos;
+    });
   }
 
+  getMemberData(vo: Vo) {
+    this.membersManagerService.getMemberByUser(vo.id, this.userId).subscribe(member =>{
+      this.resourcesManagerService.getAssignedRichResourcesWithMember(member.id).subscribe(resources =>{
+        this.resources = resources;
+      })
+    })
+  }
 }
