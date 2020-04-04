cdddfc4ea8db9df12f7ba07ac0ee2da7ae9daedb
Martin Kostka <mkostka62@gmail.com>
2020-04-04 12:29:09 +0200

    Setting samba password works
    * Samba password now correctly sets the password, so this page is fully implemented

--- apps/user-profile/src/app/pages/settings-page/settings-samba-password/settings-samba-password.component.ts
+++ apps/user-profile/src/app/pages/settings-page/settings-samba-password/settings-samba-password.component.ts
@@ -23,21 +23,25 @@ export class SettingsSambaPasswordComponent implements OnInit {
   ngOnInit() {
     this.userId = this.store.getPerunPrincipal().userId;
     this.sambaControl = new FormControl(null, [Validators.required]);
+    this.getSambaAttribute();
+  }
+
+  setSambaPassword() {
+    this.sambaAttribute.value = this.sambaControl.value;
+
+    const timestamp = new Date().getTime().toString();
+    this.usersManagerService.createAlternativePassword(this.userId,timestamp,'samba-du',this.sambaControl.value).subscribe(() => {
+      console.log('done');
+      this.sambaControl.setValue('');
+      this.getSambaAttribute();
+    });
+  }
+
+  getSambaAttribute(){
     this.attributesManagerService.getUserAttributeByName(this.userId, 'urn:perun:user:attribute-def:def:altPasswords:samba-du').subscribe(att => {
       console.log(att);
       this.sambaExists = !!att.value;
       this.sambaAttribute = att;
     });
-  }
-
-  setSambaPassword() {
-    this.sambaAttribute.value = this.sambaControl.value;
-    // this.attributesManagerService.setUserAttribute({
-    //   user: this.userId,
-    //   attribute: this.sambaAttribute
-    // }).subscribe(() => {
-    //   this.sambaControl.setValue('');
-    // });
-
   }
 }
