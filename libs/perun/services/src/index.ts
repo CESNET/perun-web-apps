ab97c6acd356b0ac369aac9770f35b58a789e1a0
Martin Kostka <mkostka62@gmail.com>
2020-05-23 17:26:28 +0200

    Lint fix

--- libs/perun/services/src/index.ts
+++ libs/perun/services/src/index.ts
@@ -7,4 +7,6 @@ export { StoreService } from './lib/store.service';
 export { InitAuthService } from './lib/init-auth.service';
 export { CustomIconService } from './lib/custom-icon.service';
 export { ApiInterceptor } from './lib/ApiInterceptor';
+export { ApiRequestConfigurationService } from './lib/api-request-configuration.service';
+export { NotificatorService } from './lib/notificator.service';
 
