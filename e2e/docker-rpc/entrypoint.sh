fdbf5b459f5370b48503a7940a666a8ebb10bc58
Pavel Břoušek <brousek@ics.muni.cz>
2023-11-24 00:01:04 +0100

    ci: switch to k8s runners and bitnami images
    also optimized (use cache, build only necessary images)
    
    (cherry picked from commit b0c634a8e49acc29291a9f0bb1d1718b20ce0ccc)

--- e2e/docker-rpc/entrypoint.sh
+++ e2e/docker-rpc/entrypoint.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
-set -e
-echo "Initiating..."
+
+echo "Waiting for Postgres..."
 until pg_isready -h db -U perun -d perun; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 >&2 echo "Postgres is up"
-exec /opt/tomcat/bin/catalina.sh run
+
+# Copyright VMware, Inc.
+# SPDX-License-Identifier: APACHE-2.0
+
+# shellcheck disable=SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purposes
+
+# Load libraries
+. /opt/bitnami/scripts/libtomcat.sh
+. /opt/bitnami/scripts/liblog.sh
+
+# Load Tomcat environment variables
+. /opt/bitnami/scripts/tomcat-env.sh
+
+info "** Starting Tomcat **"
+
+if am_i_root; then
+    exec_as_user "$TOMCAT_DAEMON_USER" "${TOMCAT_BIN_DIR}/catalina.sh" run "$@"
+else
+    exec "${TOMCAT_BIN_DIR}/catalina.sh" run "$@"
+fi
