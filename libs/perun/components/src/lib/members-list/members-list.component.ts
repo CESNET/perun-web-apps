3958c997b5e2982850071c2c003e78d47bae533b
HejdaJakub <kuba.hejda97@gmail.com>
2024-03-14 08:20:45 +0100

    refactor(admin): modify members list component to support both client/server side pagination
     * A new unified component for client/server side members list pagination was created.
     * All displayed data and diverse functionality were merged into the unified component.
     * All pages where the list of members was displayed are converted to use this new unified component.
     * Unused `consentStatus` column was removed.
     * A new member-list service was created to reuse the code of server-side pagiated implementation.

--- libs/perun/components/src/lib/members-list/members-list.component.ts
+++ libs/perun/components/src/lib/members-list/members-list.component.ts
@@ -1,54 +1,62 @@
 import {
-  AfterViewInit,
   Component,
   EventEmitter,
   Input,
-  OnChanges,
+  OnInit,
   Output,
   ViewChild,
+  ViewEncapsulation,
 } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatDialog } from '@angular/material/dialog';
-import { SelectionModel } from '@angular/cdk/collections';
-import { Member } from '@perun-web-apps/perun/openapi';
+import { MemberTreeViewDialogComponent } from '@perun-web-apps/perun/dialogs';
 import {
-  customDataSourceFilterPredicate,
-  customDataSourceSort,
+  GuiAuthResolver,
+  TableCheckbox,
+  EntityStorageService,
+} from '@perun-web-apps/perun/services';
+import {
+  MemberGroupStatus,
+  PaginatedRichMembers,
+  RichMember,
+  VoMemberStatuses,
+} from '@perun-web-apps/perun/openapi';
+import { MatDialog } from '@angular/material/dialog';
+import {
+  TABLE_ITEMS_COUNT_OPTIONS,
   downloadData,
   getDataForExport,
   getDefaultDialogConfig,
+  isMemberIndirect,
   parseEmail,
   parseFullName,
   parseLogins,
   parseOrganization,
-  TABLE_ITEMS_COUNT_OPTIONS,
+  isMemberIndirectString,
+  customDataSourceFilterPredicate,
+  customDataSourceSort,
 } from '@perun-web-apps/perun/utils';
-import {
-  ChangeMemberStatusDialogComponent,
-  MemberTreeViewDialogComponent,
-} from '@perun-web-apps/perun/dialogs';
-import {
-  GuiAuthResolver,
-  PerunTranslateService,
-  TableCheckbox,
-} from '@perun-web-apps/perun/services';
-import { ActivatedRoute } from '@angular/router';
+
+import { MatSort } from '@angular/material/sort';
+import { SelectionModel } from '@angular/cdk/collections';
 import { TableWrapperComponent } from '@perun-web-apps/perun/utils';
-import { MemberWithConsentStatus } from '@perun-web-apps/perun/models';
+import { ChangeMemberStatusOrExpirationDialogComponent } from '../change-member-status-or-expiration-dialog/change-member-status-or-expiration-dialog.component';
+import {
+  DynamicDataSource,
+  isDynamicDataSource,
+  MemberWithConsentStatus,
+  PageQuery,
+} from '@perun-web-apps/perun/models';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'perun-web-apps-members-list',
   templateUrl: './members-list.component.html',
   styleUrls: ['./members-list.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class MembersListComponent implements OnChanges, AfterViewInit {
+export class MembersListComponent implements OnInit {
   @ViewChild(TableWrapperComponent, { static: true }) child: TableWrapperComponent;
-  @Input() showGroupStatuses: boolean;
-  @Input() members: MemberWithConsentStatus[] = [];
-  @Input() searchString: string;
-  @Input() selection: SelectionModel<MemberWithConsentStatus> =
-    new SelectionModel<MemberWithConsentStatus>();
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @Input() selection: SelectionModel<RichMember>;
   @Input() displayedColumns: string[] = [
     'checkbox',
     'id',
@@ -63,41 +71,101 @@ export class MembersListComponent implements OnChanges, AfterViewInit {
     'email',
     'logins',
   ];
-  @Input() disableStatusChange = false;
-  @Input() disableExpirationChange = false;
-  @Input() allowCheckboxDisabled = true;
+  @Input() voId: number;
+  @Input() groupId: number;
+  @Input() selectedGroupStatuses: MemberGroupStatus[] = [];
+  @Input() searchString: string;
+  @Input() selectedStatuses: VoMemberStatuses[];
   @Input() tableId: string;
+  @Input() updateTable: boolean;
+  @Input() isMembersGroup: boolean;
   @Input() disableRouting = false;
-  @Input() filter = '';
+  @Input() disableStatusChange = false;
+  @Input() disableCheckbox = true;
   @Input() loading: boolean;
-  @Output() updateTable = new EventEmitter<boolean>();
+  @Output() refreshTable = new EventEmitter<void>();
+  @Output() queryChanged = new EventEmitter<PageQuery>();
+  @Output() downloadAll = new EventEmitter<{
+    format: string;
+    length: number;
+    getDataForColumnFun: (data: RichMember, column: string) => string;
+  }>();
 
-  dataSource: MatTableDataSource<MemberWithConsentStatus>;
+  expireGroupAuth: boolean;
+  expireVoAuth: boolean;
+
+  dataSource: MatTableDataSource<MemberWithConsentStatus> | DynamicDataSource<RichMember>;
   pageSizeOptions = TABLE_ITEMS_COUNT_OPTIONS;
-  disabledRouting: boolean;
-  groupId: number;
-
-  private sort: MatSort;
 
   constructor(
     private dialog: MatDialog,
     private authResolver: GuiAuthResolver,
     private tableCheckbox: TableCheckbox,
-    private route: ActivatedRoute,
-    private translateService: PerunTranslateService,
+    private entityStorage: EntityStorageService,
   ) {}
 
-  @ViewChild(MatSort, { static: true }) set matSort(ms: MatSort) {
-    this.sort = ms;
+  @Input() set members(members: MemberWithConsentStatus[] | PaginatedRichMembers) {
+    // Initialize data source with first member object passed
+    // One table instance can NOT alternate between paginated and not paginated members
+    if (!this.dataSource) {
+      this.dataSourceInit(members);
+    }
+
+    // Set up data correctly on each change
+    const paginated = this.isPaginated(members);
+    if (isDynamicDataSource(this.dataSource) && paginated) {
+      this.dataSource.data = members.data;
+      this.dataSource.count = members.totalCount;
+    } else if (!isDynamicDataSource(this.dataSource) && !paginated) {
+      this.dataSource.data = members;
+    }
   }
 
-  static getFilterDataForColumn(data: MemberWithConsentStatus, column: string): string {
+  @Input() set filter(value: string) {
+    this.dataSource.filter = value;
+  }
+
+  @Input() set displayColumns(columns: string[]) {
+    if (!this.authResolver.isPerunAdminOrObserver()) {
+      columns = columns.filter((column) => column !== 'id');
+    }
+    this.displayedColumns = columns;
+  }
+
+  ngOnInit(): void {
+    // without entity beanName the error is thrown e.g. in searcher component
+    if (this.entityStorage.getEntity()) {
+      this.expireGroupAuth = this.authResolver.isAuthorized(
+        'setMemberGroupStatus_Member_Group_MemberGroupStatus_policy',
+        [this.entityStorage.getEntity()],
+      );
+      this.expireVoAuth = this.authResolver.isAuthorized('setStatus_Member_Status_policy', [
+        this.entityStorage.getEntity(),
+      ]);
+    }
+  }
+
+  getExportDataForColumn(data: RichMember, column: string): string {
     switch (column) {
+      case 'id':
+        return data.id.toString();
+      case 'voId':
+        return data.voId.toString();
+      case 'userId':
+        return data.userId.toString();
+      case 'type':
+        return data.membershipType;
       case 'fullName':
         if (data.user) {
           return parseFullName(data.user);
         }
         return '';
+      case 'status':
+        return data.status;
+      case 'groupStatus':
+        return data.groupStatus;
+      case 'organization':
+        return parseOrganization(data);
       case 'email':
         return parseEmail(data);
       case 'logins':
@@ -107,97 +175,173 @@ export class MembersListComponent implements OnChanges, AfterViewInit {
     }
   }
 
-  static getSortDataForColumn(
-    data: MemberWithConsentStatus,
-    column: string,
-    showGroupStatuses: boolean,
-  ): string {
-    switch (column) {
-      case 'id':
-        return data.id.toString();
-      case 'fullName':
-        if (data.user) {
-          return data.user.lastName ? data.user.lastName : data.user.firstName ?? '';
-        }
-        return '';
-      case 'status':
-        return showGroupStatuses ? data.groupStatus : data.status;
-      case 'organization':
-        return parseOrganization(data);
-      case 'email':
-        return parseEmail(data);
-      case 'consentStatus':
-        return data.consent;
-      default:
-        return '';
+  getDataForColumnFun = (data: RichMember, column: string): string => {
+    return this.getExportDataForColumn(data, column);
+  };
+
+  /**
+   * Sort full name by last name or first name
+   * @param data
+   * @param column
+   */
+  getSortDataForColumnFun = (data: RichMember, column: string): string => {
+    if (column === 'fullName') {
+      if (data.user) {
+        return data.user.lastName ? data.user.lastName : data.user.firstName ?? '';
+      }
+      return '';
+    } else {
+      return this.getExportDataForColumn(data, column);
+    }
+  };
+
+  isPaginated(
+    data: MemberWithConsentStatus[] | PaginatedRichMembers,
+  ): data is PaginatedRichMembers {
+    return 'data' in data;
+  }
+
+  canBeSelected = (member: RichMember): boolean => !isMemberIndirect(member);
+
+  masterToggle(): void {
+    if (isDynamicDataSource(this.dataSource)) {
+      this.tableCheckbox.masterTogglePaginated(
+        this.dataSource,
+        this.selection,
+        !this.isAllSelected(),
+        this.canBeSelected,
+      );
+    } else {
+      this.tableCheckbox.masterToggle(
+        this.isAllSelected(),
+        this.selection,
+        this.dataSource.filter,
+        this.dataSource,
+        this.dataSource.sort,
+        this.dataSource.paginator.pageSize,
+        this.dataSource.paginator.pageIndex,
+        true,
+        this.canBeSelected,
+      );
     }
   }
 
-  getExportDataForColumn(
-    data: MemberWithConsentStatus,
-    column: string,
-    showGroupStatuses: boolean,
-  ): string {
-    switch (column) {
-      case 'id':
-        return data.id.toString();
-      case 'fullName':
-        if (data.user) {
-          return parseFullName(data.user);
-        }
-        return '';
-      case 'status':
-        return showGroupStatuses ? data.groupStatus : data.status;
-      case 'organization':
-        return parseOrganization(data);
-      case 'email':
-        return parseEmail(data);
-      case 'logins':
-        return parseLogins(data);
-      case 'consentStatus':
-        return this.translateService.instant(`CONSENTS.STATUS_${data.consent}`);
-      default:
-        return '';
+  isAllSelected(): boolean {
+    if (isDynamicDataSource(this.dataSource)) {
+      return this.tableCheckbox.isAllSelectedPaginated(
+        this.dataSource,
+        this.selection.selected.length,
+        this.canBeSelected,
+      );
+    } else {
+      return this.tableCheckbox.isAllSelected(
+        this.selection.selected.length,
+        this.dataSource,
+        this.canBeSelected,
+      );
     }
   }
 
-  getExportDataForColumnFun = (data: MemberWithConsentStatus, column: string): string => {
-    return this.getExportDataForColumn(data, column, this.showGroupStatuses);
-  };
+  /**
+   * Open the dialog for changing tho Vo/Group status
+   * Dialog can be opened if the status/groupStatus column is between displayedColumns
+   * AND the "disableStatusChange" property is set to false (default)
+   * Do NOT open this dialog if the:
+   *    1) caller doesn't have sufficient rights (for Vo or Group)
+   *    2) vo member is unalterable (member from member Vo)
+   *    3) current group is 'members'
+   *    4) group member is indirect
+   *
+   * NOTE: isLifecycleAlterable member attribute should be presented in the member object to ensure the correct behavior
+   *
+   * @param event to stop default click event
+   * @param member member for who should be changed the status
+   * @param groupId id of group if the status should be changed on a group level
+   */
+  openMembershipDialog(event: Event, member: RichMember, groupId?: number): void {
+    event.stopPropagation();
 
-  getSortDataForColumnFun = (data: MemberWithConsentStatus, column: string): string => {
-    return MembersListComponent.getSortDataForColumn(data, column, this.showGroupStatuses);
-  };
+    if (!this.disableStatusChange) {
+      const indirect = isMemberIndirectString(member);
+      if (!groupId) {
+        if (!this.expireVoAuth || indirect === 'UNALTERABLE') return;
+      } else if (!this.expireGroupAuth || this.isMembersGroup || indirect === 'INDIRECT') return;
+
+      const config = getDefaultDialogConfig();
+      config.width = '400px';
+      config.data = { member: member, voId: this.voId, groupId: groupId };
+
+      const dialogRef = this.dialog.open(ChangeMemberStatusOrExpirationDialogComponent, config);
+
+      dialogRef.afterClosed().subscribe(() => {
+        this.refreshTable.emit();
+      });
+    }
+  }
 
   exportDisplayedData(format: string): void {
-    const start = this.dataSource.paginator.pageIndex * this.dataSource.paginator.pageSize;
-    const end = start + this.dataSource.paginator.pageSize;
-    downloadData(
-      getDataForExport(
-        this.dataSource
-          .sortData(this.dataSource.filteredData, this.dataSource.sort)
-          .slice(start, end),
-        this.displayedColumns,
-        this.getExportDataForColumnFun,
-      ),
-      format,
-    );
+    if (isDynamicDataSource(this.dataSource)) {
+      downloadData(
+        getDataForExport(this.dataSource.data, this.displayedColumns, this.getDataForColumnFun),
+        format,
+      );
+    } else {
+      const start = this.dataSource.paginator.pageIndex * this.dataSource.paginator.pageSize;
+      const end = start + this.dataSource.paginator.pageSize;
+      downloadData(
+        getDataForExport(
+          this.dataSource
+            .sortData(this.dataSource.filteredData, this.dataSource.sort)
+            .slice(start, end),
+          this.displayedColumns,
+          this.getDataForColumnFun,
+        ),
+        format,
+      );
+    }
   }
 
   exportAllData(format: string): void {
-    downloadData(
-      getDataForExport(
-        this.dataSource.filteredData,
-        this.displayedColumns,
-        this.getExportDataForColumnFun,
-      ),
-      format,
-    );
+    if (isDynamicDataSource(this.dataSource)) {
+      this.downloadAll.emit({
+        format: format,
+        length: this.dataSource.paginator.length,
+        getDataForColumnFun: this.getDataForColumnFun,
+      });
+    } else {
+      downloadData(
+        getDataForExport(
+          this.dataSource.filteredData,
+          this.displayedColumns,
+          this.getDataForColumnFun,
+        ),
+        format,
+      );
+    }
   }
 
-  setDataSource(): void {
-    if (!this.dataSource) {
-      this.dataSource = new MatTableDataSource<MemberWithConsentStatus>();
+  viewMemberGroupTree(event: Event, member: RichMember): void {
+    event.stopPropagation();
+    const config = getDefaultDialogConfig();
+    config.width = '1000px';
+    config.data = { member: member, groupId: this.groupId };
+
+    this.dialog.open(MemberTreeViewDialogComponent, config);
+  }
+
+  private dataSourceInit(members: MemberWithConsentStatus[] | PaginatedRichMembers): void {
+    const paginated = this.isPaginated(members);
+
+    // Create data source based on input type
+    this.dataSource = paginated
+      ? new DynamicDataSource(members.data, members.totalCount, this.sort, this.child.paginator)
+      : new MatTableDataSource(members);
+
+    if (isDynamicDataSource(this.dataSource)) {
+      // Subscribe to data source changes and pass them to parent
+      this.dataSource.pageQuery$.subscribe((query) => this.queryChanged.emit(query));
+    } else {
+      // Initialize client-side data source
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.child.paginator;
       this.dataSource.filterPredicate = (data: MemberWithConsentStatus, filter: string): boolean =>
@@ -205,7 +349,8 @@ export class MembersListComponent implements OnChanges, AfterViewInit {
           data,
           filter,
           this.displayedColumns,
-          MembersListComponent.getFilterDataForColumn,
+          this.getDataForColumnFun,
+          true,
         );
       this.dataSource.sortData = (
         data: MemberWithConsentStatus[],
@@ -213,76 +358,5 @@ export class MembersListComponent implements OnChanges, AfterViewInit {
       ): MemberWithConsentStatus[] =>
         customDataSourceSort(data, sort, this.getSortDataForColumnFun);
     }
-    this.dataSource.filter = this.filter;
-    this.dataSource.data = this.members;
-  }
-
-  ngAfterViewInit(): void {
-    if (!this.authResolver.isPerunAdminOrObserver()) {
-      this.displayedColumns = this.displayedColumns.filter((column) => column !== 'id');
-    }
-  }
-
-  ngOnChanges(): void {
-    this.setDataSource();
-    this.disabledRouting = this.disableRouting;
-    this.route.parent?.params.subscribe((params) => {
-      if (params['groupId']) {
-        this.groupId = Number(params['groupId']);
-      }
-    });
-  }
-
-  canBeSelected = (member: MemberWithConsentStatus): boolean => member.membershipType === 'DIRECT';
-
-  isAllSelected(): boolean {
-    return this.tableCheckbox.isAllSelected<MemberWithConsentStatus>(
-      this.selection.selected.length,
-      this.dataSource,
-      this.canBeSelected,
-    );
-  }
-
-  masterToggle(): void {
-    this.tableCheckbox.masterToggle(
-      this.isAllSelected(),
-      this.selection,
-      this.filter,
-      this.dataSource,
-      this.sort,
-      this.child.paginator.pageSize,
-      this.child.paginator.pageIndex,
-      true,
-      this.canBeSelected,
-    );
-  }
-
-  //FIXME: the correct change of status or expiration is implemented in members-dynamic-list
-  // now if the status is column between displayedColumns, the "disableStatusChange" attribute is also presented
-  // so the status change through the icon on this list isn't used anywhere. If this change should be possible
-  // in the future, the same logic as in members-dynamic-list should be used (but for this purpose some additional
-  // member attributes have to be presented in MemberWithConsentStatus object - at least "isLifecycleAlterable" attribute)
-  changeStatus(event: Event, member: MemberWithConsentStatus): void {
-    event.stopPropagation();
-    if (!this.disableStatusChange) {
-      const config = getDefaultDialogConfig();
-      config.width = '500px';
-      config.data = { member: member, disableChangeExpiration: this.disableExpirationChange };
-
-      const dialogRef = this.dialog.open(ChangeMemberStatusDialogComponent, config);
-      dialogRef.afterClosed().subscribe((success: Member) => {
-        if (success) {
-          this.updateTable.emit(true);
-        }
-      });
-    }
-  }
-
-  viewMemberGroupTree(member: MemberWithConsentStatus): void {
-    const config = getDefaultDialogConfig();
-    config.width = '800px';
-    config.data = { member: member, groupId: this.groupId };
-
-    this.dialog.open(MemberTreeViewDialogComponent, config);
   }
 }
