fb11590fa58b4dcf186c920d25450dfe32a5373e
sarkapalkovicova <sarka.palkovicova@gmail.com>
2024-05-30 15:34:43 +0200

    fix(admin): data loading after the back button
    * The group managers page data are loaded for the current group according to url

--- apps/admin-gui/src/app/vos/pages/group-detail-page/group-settings/group-settings-managers/group-settings-managers.component.ts
+++ apps/admin-gui/src/app/vos/pages/group-detail-page/group-settings/group-settings-managers/group-settings-managers.component.ts
@@ -1,6 +1,7 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
-import { Group, RoleManagementRules } from '@perun-web-apps/perun/openapi';
-import { EntityStorageService, GuiAuthResolver } from '@perun-web-apps/perun/services';
+import { Group, GroupsManagerService, RoleManagementRules } from '@perun-web-apps/perun/openapi';
+import { GuiAuthResolver } from '@perun-web-apps/perun/services';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-group-settings-managers',
@@ -9,6 +10,7 @@ import { EntityStorageService, GuiAuthResolver } from '@perun-web-apps/perun/ser
 })
 export class GroupSettingsManagersComponent implements OnInit {
   @HostBinding('class.router-component') true;
+  loading = false;
   group: Group;
   availableRoles: RoleManagementRules[] = [];
   selected = 'user';
@@ -17,11 +19,23 @@ export class GroupSettingsManagersComponent implements OnInit {
 
   constructor(
     private guiAuthResolver: GuiAuthResolver,
-    private entityStorageService: EntityStorageService,
+    public route: ActivatedRoute,
+    private groupsManager: GroupsManagerService,
   ) {}
 
   ngOnInit(): void {
-    this.group = this.entityStorageService.getEntity();
-    this.guiAuthResolver.assignAvailableRoles(this.availableRoles, 'Group');
+    this.loading = true;
+    this.route.pathFromRoot[2].params.subscribe((params) => {
+      const groupId = Number(params['groupId']);
+
+      this.groupsManager.getGroupById(groupId).subscribe({
+        next: (group) => {
+          this.group = group;
+          this.guiAuthResolver.assignAvailableRoles(this.availableRoles, 'Group');
+          this.loading = false;
+        },
+        error: () => (this.loading = false),
+      });
+    });
   }
 }
